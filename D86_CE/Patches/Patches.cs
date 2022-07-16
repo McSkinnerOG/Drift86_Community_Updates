c8c28e1800dcc21be10c84e321e4a1848dc9ac6a
McSkinnerOG <40324085+McSkinnerOG@users.noreply.github.com>
2022-07-16 21:44:20 +1000

    Project Setup
    Project Setup, Just Boilerplate

---
+++ D86_CE/Patches/Patches.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+
+namespace D86_CE.Patches
+{
+    [HarmonyPatch]
+    public static class Patches
+    {
+
+    }
+}
